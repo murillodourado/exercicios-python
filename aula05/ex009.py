1f9352006a3abaccae90b56169d4f0096efcffe0
Murillo Dourado <murillodourado19@gmail.com>
2026-03-19 17:24:07 -0300

    resolvendo ex009

--- aula05/ex009.py
+++ aula05/ex009.py
@@ -1,3 +1,29 @@
 '''Login com bloqueio. solicitar senha e tentativas. Sabendo que o usuário terá até 3 tentativas.
-Solicitar a senha para ter acesso ao sistema.
+Solicitar a senha para ter acesso ao sistema. (sem usar estrutura de repetição)
 '''
+
+senha_correta = '1234'
+
+# 1ª Tentativa
+senha = input('Digite a senha: ')
+
+if senha == senha_correta:
+    print('Você entrou no sistema!')
+else:
+    print('Senha incorreta. Tentativa 2 de 3.')
+
+    # 2ª Tentativa
+    senha = input('Digite sua senha:')
+    
+    if senha == senha_correta:
+        print('Você entrou no sistema!')
+    else:
+        print('Senha incorreta. Tentativa 3 de 3.')
+
+        # 3ª Tentativa
+        senha = input('Digite sua senha: ')
+
+        if senha == senha_correta:
+            print('Você entrou no sistema!')
+        else:
+            print('Senha incorreta. Você excedeu o número de tentativas, usuário bloqueado!')
